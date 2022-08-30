172ebbd418f40a144bd809e76ea22f01906e1a73
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-08-30 09:31:57 +0100

    expense component

--- 01-starting-setup/src/components/ExpenseItem.jsx
+++ 01-starting-setup/src/components/ExpenseItem.jsx
@@ -5,14 +5,17 @@ function ExpenseItem(props) {
   //   const expenseDate = new Date(2021, 2, 28);
   //   const expenseTitle = "Car Insurance";
   //   const expenseAmount = 294.69;
+  const date = props.date;
+  const title = props.title;
+  const amount = props.amount;
 
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__price">${amount}</div>
     </div>
   );
 }
