c73204665418979149a38f0f51e404ddbbf8158b
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-08-31 17:50:15 +0100

    rework done

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -30,8 +30,7 @@ const App = () => {
 
   return (
     <div>
-      <h2> Let 's get started!</h2>
-      <Expenses items={expenses} />
+      <h2> Let 's get started!</h2> <Expenses items={expenses} />{" "}
     </div>
   );
 };
