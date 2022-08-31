52838c0eea3b7090a2cdf954c11290be4041f6ee
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-08-31 08:33:48 +0100

    file rearranging

--- 01-starting-setup/src/components/Expenses/ExpenseItem.jsx
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.jsx
@@ -1,7 +1,9 @@
+import Card from "../UI/Card";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   //   const expenseDate = new Date(2021, 2, 28);
   //   const expenseTitle = "Car Insurance";
   //   const expenseAmount = 294.69;
@@ -10,14 +12,14 @@ function ExpenseItem(props) {
   const amount = props.amount;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${amount}</div>
-    </div>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;
